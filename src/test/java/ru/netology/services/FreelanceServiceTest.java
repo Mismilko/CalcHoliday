d1147946162af19200bd6eea503288554594c7d8
M.I.S <irina.morozova.21@inbox.ru>
2023-06-03 00:21:14 +0300

    initial commit

---
+++ src/test/java/ru/netology/services/FreelanceServiceTest.java
@@ -0,0 +1,26 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.services.services.FreelanceService;
+
+class FreelanceServiceTest {
+    @Test
+    public void testHoliday3months() {
+        FreelanceService service = new FreelanceService();
+        int actual = service.calculate(10_000, 3_000, 20_000);
+        int expected = 3;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void testHoliday2months() {
+        FreelanceService service = new FreelanceService();
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        int expected = 2;
+        Assertions.assertEquals(expected, actual);
+
+
+    }
+}
