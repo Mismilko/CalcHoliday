0c58c9c821e3a28c2ddf5ac0edf80ac9c8362fc9
M.I.S <irina.morozova.21@inbox.ru>
2023-06-15 21:43:18 +0300

    ref code

--- src/main/java/ru/netology/services/services/FreelanceService.java
+++ src/main/java/ru/netology/services/services/FreelanceService.java
@@ -1,18 +1,19 @@
 package ru.netology.services.services;
-        public class FreelanceService {
-                public int calculate(int income, int expenses, int threshould) {
-                        int count = 0; // счётчик месяцев отдыха
-                        int money = 0; // количество денег на счету
-                        for (int month = 0; month < 12; month++) {
-                                if (money >= threshould) { // можем ли отдыхать?
-                                        count++; // увеличиваем счётчик месяцев отдыха
-                                        money = money - expenses;
-                                        money = money/3;
-                                } else {
-                                        money = money + income;
-                                        money = money - expenses;
-                                }
-                        }
-                        return count;
-                }
+
+public class FreelanceService {
+    public int calculate(int income, int expenses, int threshould) {
+        int count = 0; // счётчик месяцев отдыха
+        int money = 0; // количество денег на счету
+        for (int month = 0; month < 12; month++) {
+            if (money >= threshould) { // можем ли отдыхать?
+                count++; // увеличиваем счётчик месяцев отдыха
+                money = money - expenses;
+                money = money / 3;
+            } else {
+                money = money + income;
+                money = money - expenses;
+            }
         }
+        return count;
+    }
+}
